ba44e68a2d0959679178034d812c327042bd2fa5
ahadsenja <hamdanilm96@gmail.com>
2022-11-10 23:27:03 +0800

    update navbar

--- src/components/navigation/Navbar.vue
+++ src/components/navigation/Navbar.vue
@@ -7,7 +7,7 @@
     >
       <RouterLink :to="'/'" class="flex items-center">
         <img
-          src="../../assets/img/logo.svg"
+          src="@/assets/img/logo.svg"
           class="h-8 ml-3 sm:ml-0 sm:h-8"
           alt="Zullkit"
         />
